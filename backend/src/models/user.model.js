9c46af0048a5ec9aa8258e2d7f644a68d2cfc2d0
Ansh Singh <ansh28352@gmail.com>
2025-06-26 22:16:30 +0530

    Working on bcrypt and Jwt

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,8 @@
 import mongoose ,{Schema} from 'mongoose'
+import jwt from 'jsonwebtoken' // it is a bearer token
+import bcrypt from 'bcrypt'
+
+
 
 const userSchema = new Schema({
     username : {
@@ -34,7 +38,7 @@ const userSchema = new Schema({
     watchHistory :[
         {
             type : Schema.Types.ObjectId,
-            ref : "video",
+            ref : "Video",
 
         }
     ],
@@ -50,6 +54,49 @@ const userSchema = new Schema({
         timestamps : true
     }
 )
+// middle ware for encryption of password
+userSchema.pre("save" ,async function (next) {
+    if(!this.isModified("password")){
+        return next()
+    }
+        this.password = await  bcrypt.hash(this.password ,10)
+        next()
+})
 
+
+// custom method
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+        return await bcrypt.compare(password , this.password)
+}
+
+
+// methods for generating access token
+userSchema.methods.generateAccessTokem = function () {
+       return jwt.sign({
+                _id : this._id,
+                email:this.email ,
+                username : this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+        }
+    
+    )
+}
+userSchema.methods.generateRefreshTokem = function () {
+
+           return jwt.sign({
+                _id : this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+        }
+    
+    )
+
+}
 
 export default  mongoose.model('User' , userSchema)
